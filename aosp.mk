26d7150c1d2d2f3c31a6e21a37563dacef539684
Roker2 (Dmitry Minko) <dimaminko2000@gmail.com>
2018-08-15 22:58:22 +0300

    Update aosp.mk

--- aosp.mk
+++ aosp.mk
@@ -1,1 +1,15 @@
+# Inherit from device specific
+$(call inherit-product, device/asus/Z00ED/full_Z00ED.mk)
 
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/asus/msm8916-common/overlay/lineage
+
+PRODUCT_NAME := aosp_Z00ED
+PRODUCT_DEVICE := Z00ED
+PRODUCT_BRAND := asus
+PRODUCT_MODEL := Zenfone 2 Laser
+PRODUCT_MANUFACTURER := asus
+
+PRODUCT_GMS_CLIENTID_BASE := android-asus
